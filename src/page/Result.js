70a8a581256d4a35a60bc2b55de422d956eee142
nitu <nituk650@gmail.com>
2019-01-26 09:37:38 +0530

    first commit

--- src/page/Result.js
+++ src/page/Result.js
@@ -15,6 +15,23 @@ import SearchIcon from '@material-ui/icons/Search';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import MoreIcon from '@material-ui/icons/MoreVert';
 
+
+import ListItemText from '@material-ui/core/ListItemText';
+import Select from '@material-ui/core/Select';
+import Checkbox from '@material-ui/core/Checkbox';
+import Chip from '@material-ui/core/Chip';
+import Paper from '@material-ui/core/Paper';
+import classNames from 'classnames';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+
+
+
+
+
+
+
+
 const styles = theme => ({
   root: {
     width: '100%',
@@ -82,13 +99,91 @@ const styles = theme => ({
     [theme.breakpoints.up('md')]: {
       display: 'none',
     },
+
+
+
+
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    formControl: {
+      margin: theme.spacing.unit,
+      minWidth: 120,
+      maxWidth: 300,
+    },
+    chips: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    chip: {
+      margin: theme.spacing.unit / 4,
+    },
+    noLabel: {
+      marginTop: theme.spacing.unit * 3,
+    },
+
+
+
+
+
+
+
   },
 });
+
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const names = [
+  'Oliver Hansen',
+  'Van Henry',
+  'April Tucker',
+  'Ralph Hubbard',
+  'Omar Alexander',
+  'Carlos Abbott',
+  'Miriam Wagner',
+  'Bradley Wilkerson',
+  'Virginia Andrews',
+  'Kelly Snyder',
+];
+
+function getStyles(name, that) {
+  return {
+    fontWeight:
+      that.state.name.indexOf(name) === -1
+        ? that.props.theme.typography.fontWeightRegular
+        : that.props.theme.typography.fontWeightMedium,
+  };
+}
+
+
+
+
+
+
+
+
+
+
 
 class Result extends React.Component {
   state = {
     anchorEl: null,
     mobileMoreAnchorEl: null,
+
+    name: [],
+
+
   };
 
   handleProfileMenuOpen = event => {
@@ -107,6 +202,33 @@ class Result extends React.Component {
   handleMobileMenuClose = () => {
     this.setState({ mobileMoreAnchorEl: null });
   };
+
+
+
+
+  handleChange = event => {
+    this.setState({ name: event.target.value });
+  };
+
+  handleChangeMultiple = event => {
+    const { options } = event.target;
+    const value = [];
+    for (let i = 0, l = options.length; i < l; i += 1) {
+      if (options[i].selected) {
+        value.push(options[i].value);
+      }
+    }
+    this.setState({
+      name: value,
+    });
+  };
+
+
+
+
+
+
+
 
   render() {
     const { anchorEl, mobileMoreAnchorEl } = this.state;
@@ -181,12 +303,110 @@ class Result extends React.Component {
             </div>
           </Toolbar>
         </AppBar>
-        {renderMenu}
-        {renderMobileMenu}
+
+
+
+
+
+
+
+
+  render() {
+  const { classes } = this.props;
+  return (
+
+    <div className={classes.root}>
+      <Paper className={classes.Paper}>
+        <InputLabel htmlFor="select-multiple">Name</InputLabel>
+        <Select
+          multiple
+          value={this.state.name}
+          onChange={this.handleChange}
+          input={<Input id="select-multiple" />}
+          MenuProps={MenuProps}
+        >
+          {names.map(name => (
+            <MenuItem key={name} value={name} style={getStyles(name, this)}>
+              {name}
+            </MenuItem>
+          ))}
+        </Select>
+      </Paper>
+      
+      <Paper className={classes.Paper}>
+        <InputLabel htmlFor="select-multiple-checkbox">Tag</InputLabel>
+        <Select
+          multiple
+          value={this.state.name}
+          onChange={this.handleChange}
+          input={<Input id="select-multiple-checkbox" />}
+          renderValue={selected => selected.join(', ')}
+          MenuProps={MenuProps}
+        >
+          {names.map(name => (
+            <MenuItem key={name} value={name}>
+              <Checkbox checked={this.state.name.indexOf(name) > -1} />
+              <ListItemText primary={name} />
+            </MenuItem>
+          ))}
+        </Select>
+      </Paper>
+      
+
+
+      <Paper className={classNames(classes.Paper, classes.noLabel)}>
+        <Select
+          multiple
+          displayEmpty
+          value={this.state.name}
+          onChange={this.handleChange}
+          input={<Input id="select-multiple-placeholder" />}
+          renderValue={selected => {
+            if (selected.length === 0) {
+              return <em>Placeholder</em>;
+            }
+
+            return selected.join(', ');
+          }}
+          MenuProps={MenuProps}
+        >
+          <MenuItem disabled value="">
+            <em>Placeholder</em>
+          </MenuItem>
+          {names.map(name => (
+            <MenuItem key={name} value={name} style={getStyles(name, this)}>
+              {name}
+            </MenuItem>
+          ))}
+        </Select>
+        </Paper>
+    </div>
+
+        
+
+
+
+
+
+
+        
+
+{renderMenu}   
+ {renderMobileMenu}
       </div>
-    );
-  }
+
+);
 }
+}
+
+
+
+  
+
+
+
+
+
 
 Result.propTypes = {
   classes: PropTypes.object.isRequired,
